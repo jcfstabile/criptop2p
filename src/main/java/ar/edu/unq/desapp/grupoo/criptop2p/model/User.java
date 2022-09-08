299dcd2c6c6d8c817b862ee2ef377389f37cc8b4
Juan Carlos Francisco Stabile <jcfstabile@gmail.com>
2022-09-08 00:46:57 -0300

    Refact User attributes to getters

--- src/main/java/ar/edu/unq/desapp/grupoo/criptop2p/model/User.java
+++ src/main/java/ar/edu/unq/desapp/grupoo/criptop2p/model/User.java
@@ -1,7 +1,5 @@
 package ar.edu.unq.desapp.grupoo.criptop2p.model;
 
-import java.util.Date;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,26 +7,20 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-
 @Entity
 @Table(name="users")
 public class User{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public
     Long id;
 
     @Column(name="name", nullable = false)
-    public
     String name;
 
     @Column(name="surname", nullable = false)
     String surname;
 
     @Column(name="email", nullable = false, unique = true)
-    public
     String email;
 
     @Column(name="address", nullable = false)
@@ -38,17 +30,18 @@ public class User{
     String password;
 
     @Column(name="w", nullable = false, unique = true)
-    String w;
+    String walletAddress;
 
     @Column(name="cvu", nullable = false)
     String cvu;
+
     public User(String aName, String aSurname, String anEmail, String anAddress, String aPassword, String aWalletAddress, String aCvu){
         this.name = aName;
         this.surname = aSurname;
         this.email = anEmail;
         this.address = anAddress;
         this.password = aPassword;
-        this.w = aWalletAddress;
+        this.walletAddress = aWalletAddress;
         this.cvu = aCvu;
     }
 
@@ -63,4 +56,13 @@ public class User{
     public void setId(Long anId){
         this.id = anId;
     }
+
+    public String getName() { return this.name; }
+    public String getSurname() { return this.surname; }
+    public String getEmail() { return this.email; }
+    public String getAddress() { return this.address; }
+    public String getPassword() { return this.password; }
+    public String getWalletAddress() { return this.walletAddress; }
+    public String getCvu() { return this.cvu; }
+
 }
