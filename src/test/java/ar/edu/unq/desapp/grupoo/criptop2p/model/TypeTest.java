70b6cf7ef80e7cede28f7e665ecefcfb3de93f53
Juan Carlos Francisco Stabile <jcfstabile@gmail.com>
2022-09-20 21:08:56 -0300

    Refact TypeTest

--- src/test/java/ar/edu/unq/desapp/grupoo/criptop2p/model/TypeTest.java
+++ src/test/java/ar/edu/unq/desapp/grupoo/criptop2p/model/TypeTest.java
@@ -1,5 +1,6 @@
 package ar.edu.unq.desapp.grupoo.criptop2p.model;
 
+import ar.edu.unq.desapp.grupoo.criptop2p.model.builders.IntentionBuilder;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,56 +13,71 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @DisplayName("Type Tests")
 @SpringBootTest
 public class TypeTest {
+
     User anUser;
+    IntentionBuilder anyIntention = new IntentionBuilder();
     Intention anIntentionBUY;
     Intention anIntentionSELL;
     @BeforeEach
     void setUp(){
         anUser = new User("Jim", "Ken", "jk@here.dom", "1234567890", "Pepito12!", "12345678", "1111111111111111111111");
-        anIntentionBUY = new Intention(anUser, 1, new BigDecimal(2), Type.BUY, CryptoName.TRXUSDT);
-        anIntentionSELL = new Intention(anUser, 1, new BigDecimal(2), Type.SELL, CryptoName.NEOUSDT);
+        anIntentionSELL = anyIntention.withType(Type.SELL).withPrice(2).build();
+        anIntentionBUY = anyIntention.withType(Type.BUY).withPrice(2).build();
     }
 
+    @DisplayName("Type SELL give User's CVU as shippingAddress")
     @Test
-    void testTypeShellReturnTheUsersCVU(){
+    void testTypeSellReturnTheUsersCVU(){
         assertEquals(anUser.getCvu(), Type.SELL.shippingAddress(anUser));
     }
 
+    @DisplayName("Type BUY give User's walletAddress as shippingAddress")
     @Test
-    void testTypeBuyReturnTheUsersCVU(){
+    void testTypeBuyReturnTheUsersWalletAddress(){
         assertEquals(anUser.getWalletAddress(), Type.BUY.shippingAddress(anUser));
     }
 
+    @DisplayName("When the current price is greater than the Intention's BUY, verify the acceptance of an Intention to CANCELEDBYSYSTEM")
     @Test
     void testToVerifyIfanIntentionIsAceptedCanceledTheIntentionWhenTheCurrentPriceisBiggerThanIntentionPriceAndtheTypeisBUY(){
         assertEquals(Status.OFFERED, anIntentionBUY.getStatus());
         Type.BUY.verifyIfIsAcepted(anUser, anIntentionBUY, new BigDecimal(3));
         assertEquals(Status.CANCELEDBYSYSTEM, anIntentionBUY.getStatus());
     }
+
+    @DisplayName("When the current price is smaller than the Intention's, SELL verify the acceptance of an Intention to CANCELEDBYSYSTEM")
     @Test
     void testToVerifyIfanIntentionIsAceptedCanceledTheIntentionWhenTheCurrentPriceisSmallerThanIntentionPriceAndTheTypeIsSELL(){
         assertEquals(Status.OFFERED, anIntentionSELL.getStatus());
         Type.SELL.verifyIfIsAcepted(anUser, anIntentionSELL, new BigDecimal(1));
         assertEquals(Status.CANCELEDBYSYSTEM, anIntentionSELL.getStatus());
     }
+
     @Test
+    @DisplayName("When the current price is smaller than the Intention's, BUY verify the acceptance of an Intention to SOLD")
     void testToVerifyIfanIntentionModicateToSOLDTheIntentionWhenTheCurrentPriceisBiggerThanIntentionPriceAndtheTypeisSELL(){
         assertEquals(Status.OFFERED, anIntentionSELL.getStatus());
         Type.BUY.verifyIfIsAcepted(anUser, anIntentionSELL, new BigDecimal(1));
         assertEquals(Status.SOLD, anIntentionSELL.getStatus());
     }
+
+    @DisplayName("When the current price is smaller than the Intention's, SELL verify the acceptance of an Intention to SOLD")
     @Test
     void testToVerifyIfanIntentionModificateToSoldTheIntentionWhenTheCurrentPriceisSmallerThanIntentionPriceAndTheTypeIsSELLBUY(){
         assertEquals(Status.OFFERED, anIntentionBUY.getStatus());
         Type.SELL.verifyIfIsAcepted(anUser, anIntentionBUY, new BigDecimal(3));
         assertEquals(Status.SOLD, anIntentionBUY.getStatus());
     }
+
+    @DisplayName("When the current price is same of the Intention's, SELL verify the acceptance of an Intention to SOLD")
     @Test
     void testToVerifyIfanIntentionIsAceptedModificateTheIntentionStatusToSOLDWhenTheCurrentPriceisEqualsToTheIntentionPriceAndTheTypeIsSELL(){
         assertEquals(Status.OFFERED, anIntentionSELL.getStatus());
         Type.SELL.verifyIfIsAcepted(anUser, anIntentionSELL, new BigDecimal(2));
         assertEquals(Status.SOLD, anIntentionSELL.getStatus());
     }
+
+    @DisplayName("When the current price is same of the Intention's, BUY verify the acceptance of an Intention to SOLD")
     @Test
     void testToVerifyIfanIntentionIsAceptedModificateToSoldTheIntentionStatusWhenTheCurrentPriceisEqualsToTheIntentionPriceAndTheTypeIsBUY(){
         assertEquals(Status.OFFERED, anIntentionSELL.getStatus());
