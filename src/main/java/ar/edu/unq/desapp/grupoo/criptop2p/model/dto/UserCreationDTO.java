651f2aa905e7245f9d2801dab0b635701642ec9b
Juan Carlos Francisco Stabile <jcfstabile@gmail.com>
2022-09-23 21:30:50 -0300

    Refact UserCreationDTO

--- src/main/java/ar/edu/unq/desapp/grupoo/criptop2p/model/dto/UserCreationDTO.java
+++ src/main/java/ar/edu/unq/desapp/grupoo/criptop2p/model/dto/UserCreationDTO.java
@@ -1,37 +1,20 @@
 package ar.edu.unq.desapp.grupoo.criptop2p.model.dto;
 
+import javax.validation.constraints.Size;
+
 public class UserCreationDTO {
+    @Size(min= 3, max=30)
     private final String name;
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getWalletAddress() {
-        return walletAddress;
-    }
-
-    public String getCvu() {
-        return cvu;
-    }
-
+    @Size(min= 3, max=30)
     private final String surname;
     private final String email;
+    @Size(min= 10, max=30)
     private final String address;
+    @Size(min= 6)
     private final String password;
+    @Size(min= 8, max =8)
     private final String walletAddress;
+    @Size(min= 22, max =22)
     private final String cvu;
 
     public UserCreationDTO(String aName, String aSurname, String anEmail, String anAddress, String aPassword, String aWalletAddress, String aCvu){
@@ -46,5 +29,23 @@ public class UserCreationDTO {
 
     public String getName() {
         return name;
+    }
+    public String getSurname() {
+        return surname;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public String getWalletAddress() {
+        return walletAddress;
+    }
+    public String getCvu() {
+        return cvu;
     }
 }
