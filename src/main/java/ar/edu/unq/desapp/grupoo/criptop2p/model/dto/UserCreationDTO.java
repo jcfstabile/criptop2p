3059052340d2cddeca4e1ea81881dddc47e27448
Juan Carlos Francisco Stabile <jcfstabile@gmail.com>
2022-09-29 14:57:07 -0300

    Remove restriction annotations on UserCreationDTO

--- src/main/java/ar/edu/unq/desapp/grupoo/criptop2p/model/dto/UserCreationDTO.java
+++ src/main/java/ar/edu/unq/desapp/grupoo/criptop2p/model/dto/UserCreationDTO.java
@@ -1,20 +1,12 @@
 package ar.edu.unq.desapp.grupoo.criptop2p.model.dto;
 
-import javax.validation.constraints.Size;
-
 public class UserCreationDTO {
-    @Size(min= 3, max=30)
     private final String name;
-    @Size(min= 3, max=30)
     private final String surname;
     private final String email;
-    @Size(min= 10, max=30)
     private final String address;
-    @Size(min= 6)
     private final String password;
-    @Size(min= 8, max =8)
     private final String walletAddress;
-    @Size(min= 22, max =22)
     private final String cvu;
 
     public UserCreationDTO(String aName, String aSurname, String anEmail, String anAddress, String aPassword, String aWalletAddress, String aCvu){
