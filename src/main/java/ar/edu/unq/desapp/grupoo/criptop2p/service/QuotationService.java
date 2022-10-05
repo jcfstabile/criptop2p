5073f18de4bf2aa3c225d3c29a84f551add4731b
underabloodysky <valenzuelahoracioe@gmail.com>
2022-10-04 22:18:09 -0300

    Adding quotation service

---
+++ src/main/java/ar/edu/unq/desapp/grupoo/criptop2p/service/QuotationService.java
@@ -0,0 +1,21 @@
+package ar.edu.unq.desapp.grupoo.criptop2p.service;
+
+import ar.edu.unq.desapp.grupoo.criptop2p.integrations.BinanceIntegration;
+import ar.edu.unq.desapp.grupoo.criptop2p.model.CryptoName;
+import ar.edu.unq.desapp.grupoo.criptop2p.webservice.Quotation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class QuotationService {
+
+    @Autowired
+    BinanceIntegration binanceIntegrator;
+
+    public List<Quotation> allQuotations() {
+        List cryptos = Arrays.asList(CryptoName.values());
+        return cryptos.stream().map(crypto -> new Quotation(crypto, binanceIntegrator.priceOf(crypto.toString()));
+    }
+}
--- src/main/java/ar/edu/unq/desapp/grupoo/criptop2p/webservice/QuotationController.java
+++ src/main/java/ar/edu/unq/desapp/grupoo/criptop2p/webservice/QuotationController.java
@@ -1,17 +1,19 @@
 package ar.edu.unq.desapp.grupoo.criptop2p.webservice;
 
+import ar.edu.unq.desapp.grupoo.criptop2p.service.QuotationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/quotation")
 @EnableAutoConfiguration
 public class QuotationController {
+    @Autowired
+    private QuotationService quotationService;
     public List<Quotation> allQuotations() {
-        return new ArrayList<>();
+        return quotationService.allQuotations();
     }
 }
