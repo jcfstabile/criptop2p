9b15dbaae09a0d47aef5ecf70f04e49ee551babf
underabloodysky <valenzuelahoracioe@gmail.com>
2022-10-26 02:52:36 -0300

    Refactor into Integrator and move files into other folders

--- src/main/java/ar/edu/unq/desapp/grupoo/criptop2p/integrations/BinanceIntegration.java
+++ src/main/java/ar/edu/unq/desapp/grupoo/criptop2p/integrations/BinanceIntegration.java
@@ -2,6 +2,7 @@ package ar.edu.unq.desapp.grupoo.criptop2p.integrations;
 
 import ar.edu.unq.desapp.grupoo.criptop2p.model.CryptoName;
 import ar.edu.unq.desapp.grupoo.criptop2p.webservice.Quotation;
+import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,10 +13,12 @@ public class BinanceIntegration extends Integrator {
 
     public Quotation priceOf(CryptoName aCryptoName) {
         String resource = "ticker/price?symbol=" + aCryptoName.name();
-        return new Quotation(aCryptoName.name(), this.query(this.completeUrl(resource), "price"));
+        JsonNode root = this.query(this.completeUrl(resource));
+        JsonNode value = root.path("price");
+        return new Quotation(aCryptoName.name(), value.asText());
     }
 
     public String check() {
-        return this.query(this.completeUrl("ping"), "result");
+        return this.query(this.completeUrl("ping")).path("result").asText();
     }
 }
--- src/main/java/ar/edu/unq/desapp/grupoo/criptop2p/integrations/Quoter.java
+++ src/main/java/ar/edu/unq/desapp/grupoo/criptop2p/integrations/Quoter.java
@@ -1,4 +1,4 @@
-package ar.edu.unq.desapp.grupoo.criptop2p.utils;
+package ar.edu.unq.desapp.grupoo.criptop2p.integrations;
 
 import ar.edu.unq.desapp.grupoo.criptop2p.integrations.Integrator;
 import ar.edu.unq.desapp.grupoo.criptop2p.service.exceptions.BinanceQueryErrorException;
@@ -11,27 +11,13 @@ import org.springframework.web.client.RestTemplate;
 import java.io.IOException;
 import java.math.BigDecimal;
 
-public class Quoter{
-
-    private final RestTemplate restTemplate;
-    protected final String resourceUrl;
-    private final ObjectMapper mapper;
-
+public class Quoter extends Integrator{
     public Quoter() {
-        this.resourceUrl = "https://www.dolarsi.com/api/api.php?type=valoresprincipales";
-        mapper = new ObjectMapper();
-        restTemplate = new RestTemplate();
+        super("https://www.dolarsi.com/api/api.php?type=valoresprincipales");
     }
 
     public BigDecimal quotationOfUsd(){
-        ResponseEntity<String> response = restTemplate.getForEntity(this.resourceUrl, String.class);;
-        JsonNode root;
-        try {
-            root = mapper.readTree(response.getBody());
-        }
-        catch (IOException ex){
-            throw new RuntimeException("");
-        }
+        JsonNode root = this.query(this.completeUrl(""));
         JsonNode value = root.path(0);
         String quotation= value.path("casa").path("compra").asText().replaceAll(",", ".");
         return new BigDecimal(quotation);
