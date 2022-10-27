1906b327a8071279568ef896ce351b8011966b5e
Juan Carlos Francisco Stabile <jcfstabile@gmail.com>
2022-10-27 13:21:32 -0300

    Refact fake data setting

--- src/main/java/ar/edu/unq/desapp/grupoo/criptop2p/service/InitDbData.java
+++ src/main/java/ar/edu/unq/desapp/grupoo/criptop2p/service/InitDbData.java
@@ -2,14 +2,28 @@ package ar.edu.unq.desapp.grupoo.criptop2p.service;
 
 import ar.edu.unq.desapp.grupoo.criptop2p.model.builders.UserCreationDTOBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.PostConstruct;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
+@Profile("dev")
 @Service
+@Transactional
+@PropertySource("classpath:application.properties")
 public class InitDbData {
 
+    protected final Log logger = LogFactory.getLog(getClass());
+
     UserCreationDTOBuilder userBuilder;
+
+    @Value("${spring.datasource.driverClassName:NONE}")
+    private  String driverClassName;
 
     @Autowired
     UserService userService;
@@ -23,14 +37,19 @@ public class InitDbData {
     InitDbData(UserService anUserService){
         this();
         userService = anUserService;
+        driverClassName = "org.h2.Driver"; // REFACTOR to profiles
+        logger.warn("Setting driverClassName with test propose with: " + driverClassName );
     }
 
     @PostConstruct
     void init(){
-        userService.addUser(userBuilder.withEmail("email1@init.data").withWalletAddress("IDD09871").build());
-        userService.addUser(userBuilder.withEmail("email2@init.data").withWalletAddress("IDD09872").build());
-        userService.addUser(userBuilder.withEmail("email3@init.data").withWalletAddress("IDD09873").build());
-        userService.addUser(userBuilder.withEmail("email4@init.data").withWalletAddress("IDD09874").build());
+        if(driverClassName.equals("org.h2.Driver")) {
+            userService.addUser(userBuilder.withEmail("email1@init.data").withWalletAddress("IDD09871").build());
+            userService.addUser(userBuilder.withEmail("email2@init.data").withWalletAddress("IDD09872").build());
+            userService.addUser(userBuilder.withEmail("email3@init.data").withWalletAddress("IDD09873").build());
+            userService.addUser(userBuilder.withName("Alf").withEmail("email4@init.data").withWalletAddress("IDD09874").build());
+            logger.info("Added fake Users on H2 with: " + driverClassName );
+        }
     }
 
 }
