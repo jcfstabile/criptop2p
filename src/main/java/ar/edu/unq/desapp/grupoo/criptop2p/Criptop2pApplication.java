3f7193875b78bc7689a35f0840c9cc8b266b808f
Juan Carlos Francisco Stabile <jcfstabile@gmail.com>
2022-10-26 23:42:31 -0300

    Add fake data insertion mechanism

--- src/main/java/ar/edu/unq/desapp/grupoo/criptop2p/Criptop2pApplication.java
+++ src/main/java/ar/edu/unq/desapp/grupoo/criptop2p/Criptop2pApplication.java
@@ -1,10 +1,15 @@
 package ar.edu.unq.desapp.grupoo.criptop2p;
 
+import ar.edu.unq.desapp.grupoo.criptop2p.service.InitDbData;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Criptop2pApplication {
+
+	@Autowired
+	InitDbData initDbData;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Criptop2pApplication.class, args);
