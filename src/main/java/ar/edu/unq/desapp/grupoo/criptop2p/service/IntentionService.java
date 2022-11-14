cfa6ec58bf347a57e64deb5c203698d477ce7228
underabloodysky <valenzuelahoracioe@gmail.com>
2022-11-14 16:33:04 -0300

    Remove unused import in IntentionService

--- src/main/java/ar/edu/unq/desapp/grupoo/criptop2p/service/IntentionService.java
+++ src/main/java/ar/edu/unq/desapp/grupoo/criptop2p/service/IntentionService.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import javax.validation.Validator;
 import java.util.ArrayList;
 import java.util.List;
 
--- src/main/java/ar/edu/unq/desapp/grupoo/criptop2p/webservice/responses/ResponseHandler.java
+++ src/main/java/ar/edu/unq/desapp/grupoo/criptop2p/webservice/responses/ResponseHandler.java
@@ -9,32 +9,26 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 @ControllerAdvice
 public class ResponseHandler  extends ResponseEntityExceptionHandler {
-
     @ExceptionHandler({UserNotFoundException.class})
     public ResponseEntity<Object> handleUserNotFound(UserNotFoundException exception){
         return new ResponseEntity<>(new ResponseErrorSimple("001", "User not found", exception.getMessage()), HttpStatus.NOT_FOUND);
     }
-
     @ExceptionHandler({UserConstraintViolationException.class})
     public ResponseEntity<Object> handleUserConstraintViolation(UserConstraintViolationException exception){
         return new ResponseEntity<>(new ResponseErrorList("002", "User data malformed", exception.getErrors()), HttpStatus.BAD_REQUEST);
     }
-
     @ExceptionHandler({DataIncomingConflictException.class})
     public ResponseEntity<Object> handleDataConflictException(DataIncomingConflictException exception){
         return new ResponseEntity<>(new ResponseErrorSimple("003", "User was already registered", exception.getError()), HttpStatus.CONFLICT);
     }
-
     @ExceptionHandler({ServerCantHandleRequestNowException.class})
     public ResponseEntity<Object> handleServerCantHandleRequestException(ServerCantHandleRequestNowException exception){
         return new ResponseEntity<>(new ResponseErrorSimple("004", "A major error had occurred", exception.getError()), HttpStatus.SERVICE_UNAVAILABLE);
     }
-
     @ExceptionHandler({InterruptedErrorException.class})
     public ResponseEntity<Object> handleInterruptedErrorException(InterruptedErrorException exception){
         return new ResponseEntity<>(new ResponseErrorSimple("005", "A major error had occurred", exception.getMessage()), HttpStatus.SERVICE_UNAVAILABLE);
     }
-
     @ExceptionHandler({IntentionNotFoundException.class})
     public ResponseEntity<Object> handleIntentionNotFoundException(IntentionNotFoundException exception){
         return new ResponseEntity<>(new ResponseErrorSimple("006", "Intention not found", exception.getMessage()), HttpStatus.NOT_FOUND);
