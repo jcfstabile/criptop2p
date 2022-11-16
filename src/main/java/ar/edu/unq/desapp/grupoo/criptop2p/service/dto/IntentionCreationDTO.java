5c35ee5011565eb634627291586d9b1d81d8f7ca
underabloodysky <valenzuelahoracioe@gmail.com>
2022-11-16 17:25:31 -0300

    Declare attrb in separate lines in IntentionCreationDTO

--- src/main/java/ar/edu/unq/desapp/grupoo/criptop2p/service/dto/IntentionCreationDTO.java
+++ src/main/java/ar/edu/unq/desapp/grupoo/criptop2p/service/dto/IntentionCreationDTO.java
@@ -2,7 +2,10 @@ package ar.edu.unq.desapp.grupoo.criptop2p.service.dto;
 
 public class IntentionCreationDTO {
     private final int count;
-    private final String price, type, cryptoName;
+    private final String;
+    private final String price;
+    private final String type;
+    private final String cryptoName;
 
     public IntentionCreationDTO(int aCount, String aPrice, String aType, String aCryptoName) {
         this.count = aCount;
