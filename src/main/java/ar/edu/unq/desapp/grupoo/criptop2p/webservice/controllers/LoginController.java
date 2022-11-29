2892dcec6154f60cdf21f4553aa566d1c6699407
Juan Carlos Francisco Stabile <jcfstabile@gmail.com>
2022-11-29 11:17:29 -0300

    Refact login endpoint documentation
    Refact login endpoint documentation
    
    Refact login endpoint documentation

--- src/main/java/ar/edu/unq/desapp/grupoo/criptop2p/webservice/controllers/LoginController.java
+++ src/main/java/ar/edu/unq/desapp/grupoo/criptop2p/webservice/controllers/LoginController.java
@@ -1,13 +1,9 @@
 package ar.edu.unq.desapp.grupoo.criptop2p.webservice.controllers;
 
 import ar.edu.unq.desapp.grupoo.criptop2p.service.LoginService;
-import ar.edu.unq.desapp.grupoo.criptop2p.service.dto.UserDTO;
 import ar.edu.unq.desapp.grupoo.criptop2p.service.dto.UserLoginDTO;
-import ar.edu.unq.desapp.grupoo.criptop2p.webservice.responses.ResponseErrorList;
-import ar.edu.unq.desapp.grupoo.criptop2p.webservice.responses.ResponseErrorSimple;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.headers.Header;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -24,24 +20,19 @@ public class LoginController {
     LoginService loginService;
 
     @Operation(
-            summary = "Register an user",
+            summary = "Login an user",
             responses = {
-                    @ApiResponse( description = "Login sucessed", responseCode = "201",
-                            content = @Content(mediaType = "application/json",
-                                    schema = @Schema(implementation = UserDTO.class))),
-                    @ApiResponse( description = "Malformed data", responseCode = "400",
-                            content = @Content(mediaType = "application/json",
-                                    schema = @Schema(implementation = ResponseErrorList.class))),
-                    @ApiResponse( description = "User already registered", responseCode = "409",
-                            content = @Content(mediaType = "application/json",
-                                    schema = @Schema(implementation = ResponseErrorSimple.class))),
+                    @ApiResponse( description = "Login authorized", responseCode = "200",
+                            headers = { @Header(
+                                    name = "Autorization",
+                                    description = "Bearer eyEnC0dedHe4de7.eyEnC0dedj5onP4y10ad.EnC0dedVer1fy516n4ture")}
+                    ),
+                    @ApiResponse( description = "Unauthorized", responseCode = "401"),
             }
     )
-
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody UserLoginDTO userLoginDTO ){
-        String bearerToken = loginService.authenticateUser(userLoginDTO);
-        return ResponseEntity.ok().header("Authorization", bearerToken).body("Ok");
+    public ResponseEntity<Void> login(@RequestBody UserLoginDTO userLoginDTO ){
+        return ResponseEntity.ok().header("Authorization", loginService.authenticateUser(userLoginDTO)).build();
     }
 
     @GetMapping("/ping")
