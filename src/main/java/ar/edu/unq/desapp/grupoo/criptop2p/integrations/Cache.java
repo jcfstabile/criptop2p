da9397e6ce8430146b756ced49ca331cca8f2fae
underabloodysky <valenzuelahoracioe@gmail.com>
2022-11-29 12:09:39 -0300

    Remove unused import in Cache

--- src/main/java/ar/edu/unq/desapp/grupoo/criptop2p/integrations/Cache.java
+++ src/main/java/ar/edu/unq/desapp/grupoo/criptop2p/integrations/Cache.java
@@ -1,6 +1,5 @@
 package ar.edu.unq.desapp.grupoo.criptop2p.integrations;
 
-import ar.edu.unq.desapp.grupoo.criptop2p.service.InitDbData;
 import ar.edu.unq.desapp.grupoo.criptop2p.service.QuotationService;
 import ar.edu.unq.desapp.grupoo.criptop2p.service.exceptions.InternalErrorProcessingQuotationsException;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -9,7 +8,6 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
 import javax.annotation.PostConstruct;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
